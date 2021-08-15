ab2f448215a574854dae7a143295308b74c4f648
sophie_li <sop@gkj.com>
2021-08-15 22:45:09 +1000

    界面模仿

--- firstView/app/src/main/java/com/example/firstview/MainActivity.java
+++ firstView/app/src/main/java/com/example/firstview/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.firstview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        System.out.println("123");
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "hello", 300).show();
+            }
+        });
+
     }
 }
