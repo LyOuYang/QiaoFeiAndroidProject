4782f0269801af6d1988e6f2b69f0c4cd49e7ebb
sophie_li <sop@gkj.com>
2021-08-22 23:20:12 +1000

    照相

---
+++ firstView/app/src/main/java/com/example/firstview/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.firstview;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageView view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = findViewById(R.id.button8);
+        view = findViewById(R.id.image);
+        button.setOnClickListener((b)->{
+            Intent image = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(image, 1);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            Bitmap pic = data.getParcelableExtra("data");
+            view.setImageBitmap(pic);
+        }
+    }
+}
+
